d6ce934918abbe509f72e0de712f07a266e7fab8
Marucci Maximo <marucci.maximo@gmail.com>
2023-11-23 17:05:39 -0300

    feat (controller): Logic add record update in auxiliary table
    - Create controller
    - Fix minor schema

--- Controller/Adminhtml/Position/InlineEdit.php
+++ Controller/Adminhtml/Position/InlineEdit.php
@@ -1,35 +1,93 @@
 <?php
 namespace Omnipro\QuickProductPositioning\Controller\Adminhtml\Position;
 
+use Magento\Backend\App\Action;
 use Magento\Backend\App\Action\Context;
 use Magento\Framework\Controller\Result\JsonFactory;
+use Omnipro\QuickProductPositioning\Model\Position;
+use Omnipro\QuickProductPositioning\Model\PositionFactory;
 
-class InlineEdit extends \Magento\Backend\App\Action
+class InlineEdit extends Action
 {
     /** @var JsonFactory */
     private $jsonFactory;
 
+    /** @var PositionFactory */
+    private $positionFactory;
+
     /**
      * @param Context $context
      * @param JsonFactory $jsonFactory
+     * @param PositionFactory $positionFactory
      */
     public function __construct(
         Context $context,
-        JsonFactory $jsonFactory
+        JsonFactory $jsonFactory,
+        PositionFactory $positionFactory
     ) {
         parent::__construct($context);
         $this->jsonFactory = $jsonFactory;
+        $this->positionFactory = $positionFactory;
     }
 
     public function execute()
     {
         $resultJson = $this->jsonFactory->create();
-        $error = false;
         $messages = [];
+        $error = false;
+
+        if (!$this->getRequest()->getParam('isAjax')) {
+            $messages[] = __('Invalid request.');
+            $error = true;
+        } else {
+            $postItems = $this->getRequest()->getParam('items', []);
+
+            if (empty($postItems)) {
+                $messages[] = __('No data sent');
+                $error = true;
+            } else {
+                foreach ($postItems as $postData) {
+                    try {
+                        $productId = $postData['entity_id'];
+                        $positionValue = $postData['position_value'];
+                        $model = $this->positionFactory->create();
+
+                        $model->load($productId, 'product_id');
+                        if (!$model->getId()) {
+                            $model->setProductId($productId);
+                            $model->setPositionValue($positionValue);
+                        }else{
+                            $model->setPositionValue($positionValue);
+                        }
+
+                        $model->save();
+                    } catch (\Exception $e) {
+                        $messages[] = $this->getErrorWithPositionId($model, $e->getMessage());
+                        $error = true;
+                    }
+                }
+            }
+        }
 
         return $resultJson->setData([
-            'messages' => "check",
-            'error' => 100
+            'messages' => $messages,
+            'error' => $error
         ]);
+    }
+
+    /**
+     * @param Position $position
+     * @param $errorText
+     * @return string
+     */
+    private function getErrorWithPositionId(
+        Position $position,
+        $errorText
+    ) {
+        return sprintf(
+            '[Position ID %s ]: %s',
+            $position->getProductId(),
+            $errorText
+        );
     }
 }
